15e28a88c2d00b7432111fcf39ae8c977ceeb5d0
maxround <dozlobin2002@mail.ru>
2025-08-12 13:04:11 +0300

    init server, handlers, router

---
+++ cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+
+	todo "github.com/Danil-Zlo/todo-list-app"
+)
+
+func main() {
+	srv := new(todo.Server)
+	if err := srv.Run("8080"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
