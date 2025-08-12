163350d4704db18a41e2e32f283955c98f885827
maxround <dozlobin2002@mail.ru>
2025-08-12 13:50:04 +0300

    add handlers and first run server

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	todo "github.com/Danil-Zlo/todo-list-app"
+	"github.com/Danil-Zlo/todo-list-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
