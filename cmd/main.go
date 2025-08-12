8309257b7e12847ee517eebe742c8adaa9e652fb
maxround <dozlobin2002@mail.ru>
2025-08-12 15:05:49 +0300

    move to clean arch

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,28 @@ import (
 
 	todo "github.com/Danil-Zlo/todo-list-app"
 	"github.com/Danil-Zlo/todo-list-app/pkg/handler"
+	"github.com/Danil-Zlo/todo-list-app/pkg/repository"
+	"github.com/Danil-Zlo/todo-list-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") // dir name
+	viper.SetConfigName("config")  // file name
+	return viper.ReadInConfig()    // read conf file
 }
---
+++ pkg/repository/repository.go
@@ -0,0 +1,21 @@
+package repository
+
+// Интерфесы для работы с бизнес логикой
+type Authorization interface {
+}
+
+type TodoList interface {
+}
+
+type TodoItem interface {
+}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
