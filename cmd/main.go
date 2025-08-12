9d2d4d5adb0242852f911aced05d0be7d3195e86
maxround <dozlobin2002@mail.ru>
2025-08-12 22:21:29 +0300

    start description sign in handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	todo "github.com/Danil-Zlo/todo-list-app"
@@ -10,17 +9,21 @@ import (
 	"github.com/Danil-Zlo/todo-list-app/pkg/service"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	// logger
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	// load env var
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s", err.Error)
+		logrus.Fatalf("error loading env variables: %s", err.Error)
 	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
@@ -32,7 +35,7 @@ func main() {
 		Password: os.Getenv("DB_PASSWORD"),
 	})
 	if err != nil {
-		log.Fatalf("failed to initialize db: %s", err.Error())
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -41,7 +44,7 @@ func main() {
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+		logrus.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
 
--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,8 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Danil-Zlo/todo-list-app"
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
